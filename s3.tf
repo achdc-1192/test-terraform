85a1cec8a929a6f4a304315186abf9f477199679
anurag1192 <anurag.cav@gmail.com>
2021-10-08 11:09:55 -0700

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.12.31"
+
+  required_providers {
+    aws = ">= 3.50"
+  }
+}
+
+
+resource "aws_s3_bucket" "test" {
+  bucket = "my-tf-test-bucket"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
